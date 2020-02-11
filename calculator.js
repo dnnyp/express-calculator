cb1064aac319920210bf0d3d73c28959c1948117
dnnyp <dnnypham@gmail.com>
2020-02-10 20:40:25 -0500

    Initial commit

--- calculator.js
+++ calculator.js
@@ -1,51 +1,15 @@
-
 var express = require("express");
 var app = express();
 var PORT = 8080;
 
+var calculate = require('./lib/calculate')
+
 app.get('/:operator/:num1/:num2', function(req,res){
-	
 	var operator = req.params.operator
 	var num1 = parseInt(req.params.num1);
 	var num2 = parseInt(req.params.num2);
-	var result;
 
-	switch(operator){
-		case "addition":
-		result = add(num1, num2);
-		break;
-
-		case "subtraction":
-		result = subtract(num1, num2);
-		break;
-
-		case "multiplication":
-		result = multiply(num1, num2);
-		break;
-
-		case "division":
-		result = divide(num1, num2);
-		break;
-
-		default:
-		result = "Sorry, please enter a valid operator!"
-	}
-
-	function add(a, b){
-		return a + b
-	}
-
-	function subtract(a, b){
-		return a - b
-	}
-
-	function multiply(a, b){
-		return a * b
-	}
-
-	function divide(a, b){
-		return a / b
-	}
+	var result = calculate(operator, num1, num2)
 
 	res.json(result);
 });
